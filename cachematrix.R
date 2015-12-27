bea08fecaacb7cf7900a45700ef258026d2de3e8
Daniel Stuart <dancstuart@gmail.com>
2015-12-27 16:28:45 -0500

    final commit
    - update function naming for clarity
    - revise comments
    - both functions successfully tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a cached matrix object that can be used to repeatably solve the inverse of the marix, but only calculates the inverse once.
+##
+## Testing:
+##  m <- matrix(c(1, 2, 3, 4), nrow=2, ncol=2)
+##  cacheMatrix <- makeCacheMatrix(M)
+##  cacheSolve(cacheMatrix)
+##
+##  cacheMatrix$set(m)      # Change the matrix being cached.
+##  m <- cacheMatrix$get()  # Returns the matrix being cached.
 
-## Write a short comment describing this function
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    cInv <- NULL
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        cInv <<- NULL
     }
     get <- function() x
-    setmatrix <- function(solve) m <<- solve
-    getmatrix <- function() m
+    setmatrix <- function(solve) cInv <<- solve
+    getmatrix <- function() cInv
     list(set = set, get = get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    m <- x$getmatrix()
-    if(!is.null(m)) {
+    invFun <- x$getmatrix()
+    if(!is.null(invFun)) {
         message("getting cached data")
-        return(m)
+        return(invFun)
     }
     data <- x$get()
-    m <- solve(data, ...)
-    x$setmatrix(m)
-    m
+    invFun <- solve(data, ...)
+    x$setmatrix(invFun)
+    invFun
 }
